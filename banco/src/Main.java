fbe8f4824c1679adb6e4b4e66d1651ff706f9bd6
LUISA COELHO <luisacoelho.wall@gmail.com>
2026-05-11 10:51:52 -0300

    updato + inserção por Scanner

--- banco/src/Main.java
+++ banco/src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void criarTabelas(Connection conn) throws SQLException {
@@ -11,9 +12,16 @@ public class Main {
         Statement stmt = conn.createStatement();
         stmt.execute(sql);// executa o comando SQL
         stmt.close();// fecha instrução
-        System.out.println("Tabela criada com sucesso!");
     }
-    public static void insere(Connection conn, String nome, double preco, int estoque) throws SQLException{
+    public static void insere(Connection conn, Scanner sc) throws SQLException{
+        // lendo os campos
+        System.out.println("Infrome o nome do produto: ");
+        String nome = sc.nextLine();
+        System.out.println("Infrome o preco do produto: ");
+        double preco = sc.nextDouble();
+        System.out.println("Infrome a quantidade do estoque: ");
+        int estoque = sc.nextInt();
+
         //criar a inserção
         String sql = "INSERT INTO produtos(nome, preco, estoque) values(?, ?, ?)";
         // prepara uma instrução SQL
@@ -44,7 +52,11 @@ public class Main {
         }
     }
 
-    public static void deletar(Connection conn, int id) throws SQLException{
+    public static void deletar(Connection conn, Scanner sc) throws SQLException{
+        // recebendo o id do usuario
+        System.out.println("Localize o numero do id: ");
+        int id = sc.nextInt();
+
         String sql = "DELETE FROM produtos WHERE id = ?";
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setInt(1, id);
@@ -59,7 +71,14 @@ public class Main {
         }
     }// aula 05/05
 
-    public static void atualizarpreco(Connection conn, int id, double novoPreco) throws SQLException{
+    public static void atualizarpreco(Connection conn, Scanner sc) throws SQLException{
+        // recebendo novopreco e id
+        System.out.println("Qual o id para atualizar? : ");
+        int id = sc.nextInt();
+        System.out.println("Infrome o novo preco do produto: ");
+        double novoPreco = sc.nextDouble();
+
+
         String sql = "UPDATE produtos SET preco = ? WHERE id =?";
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setDouble(1, novoPreco);
@@ -70,7 +89,32 @@ public class Main {
         }else {
             System.out.println("Produto não encontrado!");
         }
-    }// aula 11/05
+    }
+
+    public static void exibirMenu(){
+        System.out.println("\n Menu CRUD de produtos");
+        System.out.println("1. Listar produtos");
+        System.out.println("2. Inserir produtos");
+        System.out.println("3. Atualizar produtos");
+        System.out.println("4. Remover produtos");
+        System.out.println("0 - Sair.");
+        System.out.println("opção? :");
+
+    }
+
+    public static void processarOpcao(Connection conn, Scanner sc, int opcao) throws SQLException{
+        switch (opcao){
+            case 1:consulta(conn); break;
+            case 2:insere(conn, sc); break;
+            case 3:atualizarpreco(conn, sc); break;
+            case 4:deletar(conn, sc); break;
+            case 0: System.out.println("Encerrado!"); break;
+            default: System.out.println("Não há essa opção!");
+        }
+    }
+
+
+    // aula 11/05
 
     public static void main(String[] args) {
         String url = "jdbc:postgresql://localhost:5432/lojaD";
@@ -78,10 +122,18 @@ public class Main {
             Connection conn = DriverManager.getConnection(url, "postgres","fatec123*");
             System.out.println("Conexão Sucedida!");
             criarTabelas(conn);
-            //insere(conn, "placa de video", 1100, 85);
-            consulta(conn);
-           // deletar(conn, 4);
-            atualizarpreco(conn, 2, 34);
+
+            // parte 2 - aula 11/05
+            Scanner sc = new Scanner(System.in);
+            int opcao = 1;
+
+            do {
+                exibirMenu();
+                opcao = sc.nextInt();
+                sc.nextLine();
+                processarOpcao(conn, sc, opcao);
+            }while (opcao != 0);
+
         }catch (SQLException e){
             System.out.println("Deu errado!" + e.getMessage());
         }

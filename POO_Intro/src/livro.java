6d8dab94e3e06404b5306644fc44af102aea9282
carol prof <luisacoelho.wall@gmail.com>
2026-08-03 17:56:26 -0300

    POO

--- POO_Intro/src/livro.java
+++ POO_Intro/src/livro.java
@@ -1,6 +1,6 @@
 public class livro{
     // variaveis de classe (colocar o statics)
-    static int qtdLivros = 0;
+    static boolean qtdLivros = false;
     static final double cod = 0.0;
 
     //variáveis de instância
@@ -9,9 +9,46 @@ public class livro{
     boolean disponivel;
 
     //Construtor padrão
+    public livro (){
+        this("Livro não cadastrado", "Autor não cadastrado", false);
+    }
 
+    public livro(String titulo, String autor, boolean disponivel){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = disponivel;
+    }
 
+    //metodos de instância
+    public void codLivro(double cod){
+        if (cod >= 500.0) cod = '!';
+    }
 
+    public void exibirLivro(){
+        System.out.println("Titulo: "+ titulo);
+        System.out.println("Autor: "+ autor);
+        System.out.println("Dispponivel?: "+ disponivel);
+    }
 
+    //metodos de classe (static)
+    public static boolean getDisponibili(){
+        return qtdLivros;
+    }
+
+    public static double getCodigos(){
+        return cod;
+    }
 
 }
+//------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+

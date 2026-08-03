793baf00aa24a80ea0c4cd9a8428d322462b3e9c
carol prof <luisacoelho.wall@gmail.com>
2026-08-03 17:14:55 -0300

    POO

---
+++ POO_Intro/src/livro.java
@@ -0,0 +1,17 @@
+public class livro{
+    // variaveis de classe (colocar o statics)
+    static int qtdLivros = 0;
+    static final double cod = 0.0;
+
+    //variáveis de instância
+    String titulo;
+    String autor;
+    boolean disponivel;
+
+    //Construtor padrão
+
+
+
+
+
+}

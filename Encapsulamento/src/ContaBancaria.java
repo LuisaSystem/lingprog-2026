6279dc7807678d6c143281cf503d8061eb7391a6
carol prof <luisacoelho.wall@gmail.com>
2026-08-16 13:37:24 -0300

    encapsulamento

--- Encapsulamento/src/ContaBancaria.java
+++ Encapsulamento/src/ContaBancaria.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class ContaBancaria {
+    public static void main(String[] args) {
+        conta gmod = new conta("Lucas GM", "2003", 1000.50);
+
+        conta.depositar(300.0);
+        conta.sacar(150.0);
+
+        //leitura sempre via getters
+        System.out.println("Titular: " + conta.getTitular());
+        System.out.println("Saldo: R$ " + conta.getSaldo());
+
+        //alteração controlada via getter , com avaliação
+        conta.getLimiteChequeEspecial();
+        conta.getLimiteChequeEspecial(); //tejeitado
+
+        // saldo não compila pq ele é PRIVATE.
+    }
 }
--- Encapsulamento/src/conta.java
+++ Encapsulamento/src/conta.java
@@ -1,35 +1,35 @@
 public class conta{
     private static int totalcontas = 0;
 
-    private String titular;
-    private String numero;
-    private double saldo;
-    private double LimiteChequeEspecial;
+    private static String titular;
+   // private String numero;
+    private static double saldo;
+    private static double LimiteChequeEspecial;
 
     public conta(String titular, String numero, double saldoInicial){
         this.titular = titular;
-        this.numero = numero;
+      //  this.numero = numero;
         this.saldo = saldoInicial;
         setLimiteChequeEspecial(0.0); // nasceu validado!
         totalcontas++;
     }
 
-     public void depositar(double valor){
+     public static void depositar(double valor){
         if(valor > 0) setSaldo(saldo + valor);
      }
 
-     public void sacar(double valor){
+     public static void sacar(double valor){
         if(valor > 0) setSaldo(saldo - valor);
      }
 
      //getters - leitura
-    public String getTitular(){return titular;}
-    public double getSaldo(){return saldo;}
-    public double getLimiteChequeEspecial(){return LimiteChequeEspecial;}
+    public static String getTitular(){return titular;}
+    public static double getSaldo(){return saldo;}
+    public static double getLimiteChequeEspecial(){return LimiteChequeEspecial;}
 
-    private  void setSaldo(double x){
+    private static void setSaldo(double x){
         if (x >= 0){
-            this.saldo = x;
+            saldo = x;
         }else {
             System.out.println("saldo inválido");
         }
